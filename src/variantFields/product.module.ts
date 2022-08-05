443576c5a9f3a79a60213f9fab3c0fedac93acb0
JasurbekYuldashov <jasurbekyuldashov123@gmail.com>
2022-08-06 04:33:14 +0500

    somechanges

---
+++ src/variantFields/product.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { GRPC_PRODUCT_PACKAGE } from './constants';
+import { productMsUrl } from 'src/shared/constants/msUrls';
+
+@Module({
+  imports: [
+    HttpModule,
+    ClientsModule.register([
+      {
+        name: GRPC_PRODUCT_PACKAGE,
+        transport: Transport.GRPC,
+        options: {
+          package: 'product',
+          protoPath: join(process.cwd(), '../helper-proto/product.proto'),
+          url: productMsUrl,
+          loader: {
+            objects: true,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ProductController],
+  providers: [],
+})
+export class ProductModule {}
